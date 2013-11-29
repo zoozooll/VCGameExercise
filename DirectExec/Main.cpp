e8853bb77db22e2d0027cba4534a6d4ad09df386
Aaron Lee <kangkang365@gmail.com>
2013-11-29 22:21:12 +0800

    1.Modified the project include files 2.Draw line exercise. 	modified:   DirectExec/DirectExec.vcxproj 	modified:   DirectExec/Main.cpp 	modified:   DirectExec/WinMain.h

--- DirectExec/Main.cpp
+++ DirectExec/Main.cpp
@@ -1,47 +1,44 @@
 #include "WinMain.h"
 
-int WINAPI WinMain(HINSTANCE hInst, HINSTANCE prevhInst, LPSTR cmdLine, int show)
-{
-	WNDCLASSEX wc = { 
-		sizeof(WNDCLASSEX),
-		CS_CLASSDC,
-		MsgProc,
-		0L,
-		0L,
-		GetModuleHandle(NULL),
-		NULL,
-		NULL,
-		NULL,
-		NULL,
-		WINDOW_CLASS,
-		NULL
-	};
-	RegisterClassEx(&wc);
-	HWND hWnd = CreateWindow(WINDOW_CLASS, WINDOW_NAME, WS_OVERLAPPEDWINDOW,
-		100, 100, WINDOW_WIDTH, WINDOW_HEIGHT,
-		GetDesktopWindow(), NULL, wc.hInstance, NULL);
-	if (InitializeD3D(hWnd, false))
-	{
-		ShowWindow(hWnd, SW_SHOWDEFAULT);
-		UpdateWindow(hWnd);
-		MSG msg;
-		ZeroMemory(&msg, sizeof(msg));
-		while(msg.message != WM_QUIT)
-		{
-			if(PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE))
-			{
-				TranslateMessage(&msg);
-				DispatchMessage(&msg);
-			} else 
-			{
-				RenderScene();
-			}
-		}
-		Shutdown();
-		UnregisterClass(WINDOW_CLASS, wc.hInstance);
-		return 0;
-	}
-}
+
+ 
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE prevhInst, LPSTR cmdLine, int show)  
+{  
+	// Register the window class  
+	WNDCLASSEX wc = { sizeof(WNDCLASSEX), CS_CLASSDC, MsgProc, 0L, 0L,  
+		GetModuleHandle(NULL), NULL, NULL, NULL, NULL,  
+		WINDOW_CLASS, NULL };  
+	RegisterClassEx(&wc);  
+	// Create the application's window  
+	HWND hWnd = CreateWindow(WINDOW_CLASS, WINDOW_NAME, WS_OVERLAPPEDWINDOW,  
+		100, 100, WINDOW_WIDTH, WINDOW_HEIGHT, GetDesktopWindow(), NULL,  
+		wc.hInstance, NULL);  
+	// Initialize Direct3D  
+	if(InitializeD3D(hWnd, false))  
+	{  
+		// Show the window  
+		ShowWindow(hWnd, SW_SHOWDEFAULT);  
+		UpdateWindow(hWnd);  
+		// Enter the message loop  
+		MSG msg;  
+		ZeroMemory(&msg, sizeof(msg));  
+		while(msg.message != WM_QUIT)  
+		{  
+			if(PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE))  
+			{  
+				TranslateMessage(&msg);  
+				DispatchMessage(&msg);  
+			}  
+			else  
+				RenderScene();  
+		}  
+	}  
+	// Release any and all resources.  
+	Shutdown();  
+	// Unregister our window.  
+	UnregisterClass(WINDOW_CLASS, wc.hInstance);  
+	return 0;  
+}  
 
 LRESULT WINAPI MsgProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)  
 {  
@@ -56,7 +53,7 @@ LRESULT WINAPI MsgProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 		break;  
 	}  
 	return DefWindowProc(hWnd, msg, wParam, lParam);  
-}
+} 
 
 bool InitializeD3D(HWND hWnd, bool fullscreen)  
 {  
@@ -73,11 +70,14 @@ bool InitializeD3D(HWND hWnd, bool fullscreen)
 	if(fullscreen)  
 	{  
 		d3dpp.Windowed = FALSE;  
-		d3dpp.BackBufferWidth = 640;  
-		d3dpp.BackBufferHeight = 480;  
+		d3dpp.BackBufferWidth = WINDOW_WIDTH;  
+		d3dpp.BackBufferHeight = WINDOW_HEIGHT;  
 	}  
 	else  
-		d3dpp.Windowed = TRUE;  
+	{
+		d3dpp.Windowed = TRUE;
+	}
+		  
 	d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;  
 	d3dpp.BackBufferFormat = displayMode.Format;  
 	// Create the D3DDevice  
@@ -86,14 +86,63 @@ bool InitializeD3D(HWND hWnd, bool fullscreen)
 	{  
 		return false;  
 	}  
-	return true;  
+	if (!InitializeObjects()) 
+	{
+		return false;
+	}
+ 	return true;  
 }  
+
+bool InitializeObjects()
+{
+	unsigned long col[3] = 
+	{
+		D3DCOLOR_XRGB(255, 0, 0),
+		D3DCOLOR_XRGB(0, 255, 0),
+		D3DCOLOR_XRGB(0, 0, 255)
+	};
+
+	Vertex objData[] = 
+	{
+		{ 420.f, 150.f, 0.5f, 1.f, col[0] },
+		{ 420.f, 350.f, 0.5f, 1.f, col[1] },
+		{ 220.f, 150.f, 0.5f, 1.f, col[2] },
+		{ 220.f, 350.f, 0.5f, 1.f, col[0]+col[1] }
+	};
+
+	// Create the vertex buffer.  
+   if(FAILED(g_D3DDevice->CreateVertexBuffer(sizeof(objData), 0,  
+             D3DFVF_VERTEX, D3DPOOL_DEFAULT, &g_D3DVertexBuffer,  
+             NULL))) return false;  
+  
+   // Fill the vertex buffer.  
+   void *ptr;  
+  
+   if(FAILED(g_D3DVertexBuffer->Lock(0, sizeof(objData),  
+      &ptr, 0))) return false;  
+  
+   memcpy(ptr, objData, sizeof(objData));  
+    
+  
+   g_D3DVertexBuffer->Unlock();  
+  
+   return true; 
+}
+
 void RenderScene()  
 {  
 	// Clear the backbuffer.  
 	g_D3DDevice->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0,0,0), 1.0f, 0);  
 	// Begin the scene. Start rendering.  
 	g_D3DDevice->BeginScene();  
+
+	// Render object.  
+    g_D3DDevice->SetStreamSource(0, g_D3DVertexBuffer, 0,  
+                                  sizeof(Vertex));  
+    g_D3DDevice->SetFVF(D3DFVF_VERTEX);  
+    g_D3DDevice->DrawPrimitive(D3DPT_LINELIST, 0, 2);  
+	
+	  
 	// End the scene. Stop rendering.  
 	g_D3DDevice->EndScene();  
 	// Display the scene.  
@@ -101,6 +150,12 @@ void RenderScene()
 }  
 void Shutdown()  
 {  
-	if(g_D3DDevice != NULL) g_D3DDevice->Release();  
-	if(g_D3D != NULL) g_D3D->Release();  
-}  
+	if(g_D3DDevice != NULL)
+	{
+		g_D3DDevice->Release(); 
+	}  
+	if(g_D3D != NULL)
+	{
+		g_D3D->Release(); 
+	} 
+} 
--- DirectExec/WinMain.h
+++ DirectExec/WinMain.h
@@ -1,16 +1,24 @@
 #include <d3d9.h>
-#pragma comment(lib, "d3d9.lib")
-#pragma comment(lib, "d3dx9.lib")
 
 #define WINDOW_CLASS "UGPDX"
 #define WINDOW_NAME "Direct3D Exercise"
 #define WINDOW_WIDTH 640
 #define WINDOW_HEIGHT 480
 
+#define D3DFVF_VERTEX (D3DFVF_XYZRHW | D3DFVF_DIFFUSE)
+
+struct Vertex
+{
+	float x, y, z, rhw;
+	unsigned long color;
+};
+
 LRESULT WINAPI MsgProc(HWND, UINT, WPARAM, LPARAM);
 bool InitializeD3D(HWND hWnd, bool fullscreen);
+bool InitializeObjects();
 void RenderScene();
 void Shutdown();
 
 LPDIRECT3D9 g_D3D;
 LPDIRECT3DDEVICE9 g_D3DDevice;
+LPDIRECT3DVERTEXBUFFER9 g_D3DVertexBuffer;
