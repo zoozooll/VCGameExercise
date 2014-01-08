4e142d80ddb1782a27433506676c4b0d8d07a668
Aaron Lee <kangkang365@gmail.com>
2014-01-08 19:22:18 +0800

    Added directx3d projec 	modified:   DirectExec/DirectExec.vcxproj 	modified:   DirectExec/Main.cpp 	modified:   DirectExec/WinMain.h

--- DirectExec/Main.cpp
+++ DirectExec/Main.cpp
@@ -2,20 +2,16 @@
 
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE prevhInst, LPSTR cmdLine, int show)
 {
-	WNDCLASSEX wc = { 
-		sizeof(WNDCLASSEX),
-		CS_CLASSDC,
-		MsgProc,
-		0L,
-		0L,
-		GetModuleHandle(NULL),
-		NULL,
-		NULL,
-		NULL,
-		NULL,
-		WINDOW_CLASS,
-		NULL
-	};
+	WNDCLASSEX wc;
+	ZeroMemory(&wc, sizeof(WNDCLASSEX));
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc = MsgProc;
+	wc.hInstance = hInst;
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+	wc.lpszClassName = WINDOW_CLASS;
+
 	RegisterClassEx(&wc);
 	HWND hWnd = CreateWindow(WINDOW_CLASS, WINDOW_NAME, WS_OVERLAPPEDWINDOW,
 		100, 100, WINDOW_WIDTH, WINDOW_HEIGHT,
@@ -26,6 +22,8 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE prevhInst, LPSTR cmdLine, int show
 		UpdateWindow(hWnd);
 		MSG msg;
 		ZeroMemory(&msg, sizeof(msg));
+		
+		// use peekmessage for loop
 		while(msg.message != WM_QUIT)
 		{
 			if(PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE))
@@ -34,13 +32,22 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE prevhInst, LPSTR cmdLine, int show
 				DispatchMessage(&msg);
 			} else 
 			{
+				// run game code here
 				RenderScene();
 			}
 		}
+		/*
+		// use the getmessage for loop
+		while (GetMessage(&msg, NULL, 0 ,0 ))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}*/
 		Shutdown();
 		UnregisterClass(WINDOW_CLASS, wc.hInstance);
-		return 0;
+		
 	}
+	return 0;
 }
 
 LRESULT WINAPI MsgProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)  
@@ -62,10 +69,10 @@ bool InitializeD3D(HWND hWnd, bool fullscreen)
 {  
 	D3DDISPLAYMODE displayMode;  
 	// Create the D3D object.  
-	g_D3D = Direct3DCreate9(D3D_SDK_VERSION);  
-	if(g_D3D == NULL) return false;  
+	d3d = Direct3DCreate9(D3D_SDK_VERSION);  
+	if(d3d == NULL) return false;  
 	// Get the desktop display mode.  
-	if(FAILED(g_D3D->GetAdapterDisplayMode(D3DADAPTER_DEFAULT, &displayMode)))  
+	if(FAILED(d3d->GetAdapterDisplayMode(D3DADAPTER_DEFAULT, &displayMode)))  
 		return false;  
 	// Set up the structure used to create the D3DDevice  
 	D3DPRESENT_PARAMETERS d3dpp;  
@@ -73,34 +80,73 @@ bool InitializeD3D(HWND hWnd, bool fullscreen)
 	if(fullscreen)  
 	{  
 		d3dpp.Windowed = FALSE;  
-		d3dpp.BackBufferWidth = 640;  
-		d3dpp.BackBufferHeight = 480;  
+		
 	}  
-	else  
-		d3dpp.Windowed = TRUE;  
+	else 
+	{
+		d3dpp.Windowed = TRUE; 
+	}
 	d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;  
-	d3dpp.BackBufferFormat = displayMode.Format;  
+	d3dpp.hDeviceWindow = hWnd;
+	d3dpp.BackBufferFormat = displayMode.Format;
+	d3dpp.BackBufferWidth = WINDOW_WIDTH;  
+	d3dpp.BackBufferHeight = WINDOW_HEIGHT;  
+
 	// Create the D3DDevice  
-	if(FAILED(g_D3D->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd,  
-		D3DCREATE_SOFTWARE_VERTEXPROCESSING, &d3dpp, &g_D3DDevice)))  
+	if(FAILED(d3d->CreateDevice(D3DADAPTER_DEFAULT, 
+		D3DDEVTYPE_HAL, 
+		hWnd,  
+		D3DCREATE_SOFTWARE_VERTEXPROCESSING, 
+		&d3dpp, 
+		&d3ddev)))  
 	{  
 		return false;  
-	}  
+	}
+	init_graphics();
 	return true;  
-}  
+}
+
+void init_graphics(void)
+{
+	CUSTOMVERTEX vertices[] = 
+	{
+		{400.f, 62.5f, 500.0f, 1.f, D3DCOLOR_XRGB(0, 0, 255)},
+		{650.f, 500.f, 500.0f, 1.f, D3DCOLOR_XRGB(0, 255, 0)},
+		{150.f, 500.f, 500.0f, 1.f, D3DCOLOR_XRGB(255, 0, 0)}
+	};
+	d3ddev->CreateVertexBuffer(3 * sizeof(CUSTOMVERTEX),
+		0,
+		CUSTOMFVF,
+		D3DPOOL_MANAGED,
+		&v_buffer,
+		NULL);
+	VOID *pVoid;
+
+	v_buffer->Lock(0, 0, (void**)&pVoid, 0);
+	memcpy(pVoid, vertices, sizeof(vertices));
+	v_buffer->Unlock();
+}
+
 void RenderScene()  
 {  
 	// Clear the backbuffer.  
-	g_D3DDevice->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0,0,0), 1.0f, 0);  
+	d3ddev->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0, 0, 0), 1.0f, 0);  
 	// Begin the scene. Start rendering.  
-	g_D3DDevice->BeginScene();  
+	d3ddev->BeginScene();  
+
+	d3ddev->SetFVF(CUSTOMFVF);
+	d3ddev->SetStreamSource(0, v_buffer, 0, sizeof(CUSTOMVERTEX));
+	d3ddev->DrawPrimitive(D3DPT_TRIANGLELIST, 0, 1);
+
 	// End the scene. Stop rendering.  
-	g_D3DDevice->EndScene();  
+	d3ddev->EndScene();  
 	// Display the scene.  
-	g_D3DDevice->Present(NULL, NULL, NULL, NULL);  
+	d3ddev->Present(NULL, NULL, NULL, NULL);  
 }  
+
 void Shutdown()  
 {  
-	if(g_D3DDevice != NULL) g_D3DDevice->Release();  
-	if(g_D3D != NULL) g_D3D->Release();  
+	if(d3ddev != NULL) d3ddev->Release();  
+	if(d3d != NULL) d3d->Release();  
+	if(v_buffer != NULL) v_buffer->Release();
 }  
--- DirectExec/WinMain.h
+++ DirectExec/WinMain.h
@@ -4,13 +4,22 @@
 
 #define WINDOW_CLASS "UGPDX"
 #define WINDOW_NAME "Direct3D Exercise"
-#define WINDOW_WIDTH 640
-#define WINDOW_HEIGHT 480
+#define WINDOW_WIDTH 1200
+#define WINDOW_HEIGHT 800
+#define CUSTOMFVF (D3DFVF_XYZRHW | D3DFVF_DIFFUSE)
 
 LRESULT WINAPI MsgProc(HWND, UINT, WPARAM, LPARAM);
 bool InitializeD3D(HWND hWnd, bool fullscreen);
-void RenderScene();
-void Shutdown();
+void RenderScene(void);
+void init_graphics(void);
+void Shutdown(void);
 
-LPDIRECT3D9 g_D3D;
-LPDIRECT3DDEVICE9 g_D3DDevice;
+LPDIRECT3D9 d3d;		// The pointer to our Direct3D interface
+LPDIRECT3DDEVICE9 d3ddev;		// The pointer to device class
+LPDIRECT3DVERTEXBUFFER9 v_buffer;    // The pointer to vertex buffer
+
+struct CUSTOMVERTEX
+{
+	FLOAT x, y, z, rhw;
+	DWORD color;
+};
